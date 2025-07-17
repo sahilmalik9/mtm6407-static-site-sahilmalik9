b0f80bccea0a79b39df7f994c1083d9f76e6b5e7
sahilmalik9 <sahilmalik9390@gmail.com>
2025-07-16 21:02:48 -0400

    Final project version - completed static site

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+  // Add a custom "date" filter
+  eleventyConfig.addFilter("date", (value, format = "yyyy") => {
+    if (value === "now") {
+      return DateTime.now().toFormat(format);
+    }
+    return DateTime.fromJSDate(value).toFormat(format);
+  });
+
+  // Copy the css folder to output
+  eleventyConfig.addPassthroughCopy("src/css");
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "dist"
+    }
+  };
+};
