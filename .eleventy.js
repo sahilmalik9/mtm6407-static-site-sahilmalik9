5fd24ca641c0d4859d0bdc71f7bf027d28d45c74
sahilmalik9 <sahilmalik9390@gmail.com>
2025-07-30 13:21:34 -0400

    Force push working backup folder to reset repo

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+const { DateTime } = require("luxon");
+
+module.exports = function(eleventyConfig) {
+  // Add custom 'date' filter for formatting dates
+  eleventyConfig.addFilter("date", (value, format = "LLLL d, yyyy") => {
+    if (value === "now") {
+      return DateTime.now().toFormat(format);
+    }
+    return DateTime.fromJSDate(value).toFormat(format);
+  });
+
+  // Copy CSS files to output folder
+  eleventyConfig.addPassthroughCopy("src/css");
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "docs"
+    }
+  };
+};
---
+++ README.md
@@ -0,0 +1,6 @@
+# MTM6407 Static Site Generator Project
+
+**Name:** Sahil Malik  
+**Student Number:** 90541145235  
+**Static Site Generator Used:** Eleventy
+
